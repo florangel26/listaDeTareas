f4f665cbfd42b2c395e177d6aef6121ae8d05f8d
Florangel Orochena <flory26991@gmail.com>
2022-04-09 12:41:12 -0400

    'listado'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>LISTADO DE TAREAS</h1>
+    <label>
+      <input
+        type="text"
+        v-model="tarea"
+        placeholder="Ingrese una nueva tarea"
+      />
+    </label>
+    <button
+      type="button"
+      id="boton"
+      class="btn btn-primary mx-2"
+      @click="addTarea"
+    >
+      Crear
+    </button>
+    <h2>Tareas</h2>
+    <ul v-for="(tarea, index) in tareas" :key="index" class="list-group">
+      <li class="list-group-item">
+        <strong>{{ index }}: {{ tarea }}</strong>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      tarea: "",
+      tareas: [],
+    };
+  },
+  methods: {
+    addTarea() {
+      const { tarea } = this;
+      this.tareas.push(tarea);
+      this.tarea = "";
+    },
   },
 };
 </script>
 
 <style>
+#all {
+  background-image: url(assets/fondo.jpg);
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  width: 50%;
+  min-height: 500px;
+  text-overflow: clip;
+  background-color: rgb(255, 253, 250);
+  font-family: "Grape Nuts", cursive;
   text-align: center;
-  color: #2c3e50;
+  color: #333;
   margin-top: 60px;
+}
+h1 {
+  margin: 20px;
+}
+.list-group {
+  width: 300px;
+  margin: 0 auto;
+}
+h2 {
+  display: block;
+  text-align: center;
+  font-size: 25px;
+}
+#boton {
+  width: 80px;
+  margin: 0 auto;
+  background-color: black;
 }
 </style>
